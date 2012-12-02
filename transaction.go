123ac11ebc2e38cf7f1efc6d46fee2b2e913bbf2
Michael Meier <distributed@gloenggi.ch>
2012-12-02 21:52:56 +0100

    changed Transact8x8 signature to include bytes read, written. added implementation

--- transaction.go
+++ transaction.go
@@ -5,36 +5,99 @@ import (
 )
 
 type Transactor8x8 interface {
-	Transact8x8(addr Addr, regaddr uint8, w []byte, r []byte) error // TODO: nwrriten, nread?
+	Transact8x8(addr Addr, regaddr uint8, w []byte, r []byte) (nw, nr int, err error)
 }
 
 type transactor8x8 struct {
 	m I2CMaster
 }
 
-func (t transactor8x8) Transact8x8(addr Addr, regaddr uint8, w []byte, r []byte) error {
+func NewTransact8x8(m I2CMaster) Transactor8x8 {
+	return transactor8x8{m}
+}
+
+func (t transactor8x8) Transact8x8(addr Addr, regaddr uint8, w []byte, r []byte) (int, int, error) {
 	m := t.m
 
+	nr := 0
+	nw := 0
+
 	if addr.GetAddrLen() != 7 {
-		return errors.New("only 7 bit addresses are supported")
+		return nw, nr, errors.New("Transact8x8: only 7 bit addresses are supported")
 	}
 
 	if err := m.Start(); err != nil {
-		return err
+		return nw, nr, err
 	}
 
-	addrb := uint8(addr.GetBaseAddr() << 1)
-	if err := m.WriteByte(addrb); err != nil {
-		if err == NACKReceived {
-			return NoSuchDevice
+	// inner function handles the whole transaction between
+	// but not including the start and the stop bit
+	err := func() error {
+		// address device
+		addrb := uint8(addr.GetBaseAddr() << 1)
+		if err := m.WriteByte(addrb); err != nil {
+			if err == NACKReceived {
+				return NoSuchDevice
+			}
+			return err
 		}
-		return err
+
+		// write regaddr
+		if err := m.WriteByte(regaddr); err != nil {
+			return err
+		}
+
+		// write w
+		for _, b := range w {
+			if err := m.WriteByte(b); err != nil {
+				return err
+			}
+
+			nw++
+		}
+
+		// read part of transaction is only performed if desired
+		if len(r) > 0 {
+			// start again
+			if err := m.Start(); err != nil {
+				return err
+			}
+
+			// write device's read address
+			if err := m.WriteByte(addrb | 0x01); err != nil {
+				if err == NACKReceived {
+					return NoSuchDevice
+				}
+				return err
+			}
+
+			for i := 0; i < len(r); i++ {
+				ack := true
+				if i == len(r)-1 {
+					ack = false
+				}
+				rb, err := m.ReadByte(ack)
+				if err != nil {
+					return err
+				}
+
+				r[i] = rb
+
+				nr++
+			}
+
+		}
+
+		return nil
+	}()
+
+	if err != nil {
+		// if there already was an error, the error from stop is ignored
+		// and the first error is reported
+		m.Stop()
+	} else {
+		err = m.Stop()
 	}
 
-	// write regaddr
-	// write w
-	// start again
-	// write device addr
-	// read into r
-	return errors.New("not implemented")
+	return nw, nr, err
 }
